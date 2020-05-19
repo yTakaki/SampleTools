5666afe8836955e3c9cc2c66a2957171a332d39a
yTakaki <yakkosan716@gmail.com>
2020-05-19 15:50:29 +0900

    reset SignupForm validation

--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -1,26 +1,15 @@
 package com.example.demo.login.domain.model;
 
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-
-import org.hibernate.validator.constraints.Length;
-
 import lombok.Data;
 
 @Data
 public class SignupForm {
 
-	@NotBlank
-	@Email
 	private String userId;
 
-	@NotBlank
-	@Length(min=4,max=16)
 	private String password;
 
-	@NotBlank
-	@Length(min=1,max=20)
 	private String userName;
 
 }
