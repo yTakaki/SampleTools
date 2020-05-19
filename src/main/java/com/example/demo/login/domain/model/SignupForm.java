b18cfa4ee492dbee2e7cddb6a1f7c9c153ab3bd1
yTakaki <yakkosan716@gmail.com>
2020-05-19 16:29:58 +0900

    update Validation Function

--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -1,15 +1,24 @@
 package com.example.demo.login.domain.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
 public class SignupForm {
 
+	@NotBlank
+	@Email
 	private String userId;
 
+	@NotBlank
+	@Size(min=4,max=12)
 	private String password;
 
+	@NotBlank
+	@Size(min=1,max=20)
 	private String userName;
 
 }
