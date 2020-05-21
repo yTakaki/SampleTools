a31721f1ef55691ada1aaa37871dfbf4ec720d1d
yTakaki <yakkosan716@gmail.com>
2020-05-21 14:20:38 +0900

    add User

---
+++ src/main/java/com/example/demo/login/domain/model/User.java
@@ -0,0 +1,13 @@
+package com.example.demo.login.domain.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+	private String userId;
+
+	private String password;
+
+	private String userName;
+}
