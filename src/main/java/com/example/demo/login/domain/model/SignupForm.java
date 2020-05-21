b5f6f04c163b66fd611150a38e96f9ccebad2764
yTakaki <yakkosan716@gmail.com>
2020-05-21 11:50:23 +0900

    update SignupForm and SignupFormTest

--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -2,6 +2,7 @@ package com.example.demo.login.domain.model;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -13,9 +14,11 @@ public class SignupForm {
 	private String userId;
 
 	@NotBlank
+	@Size(min=4,max=16)
 	private String password;
 
 	@NotBlank
+	@Size(max=20)
 	private String userName;
 
 }
--- src/test/java/com/example/demo/login/domain/model/SignupFormTest.java
+++ src/test/java/com/example/demo/login/domain/model/SignupFormTest.java
@@ -34,10 +34,59 @@ public class SignupFormTest {
 	}
 
 	@Test
-	void userIdがnull() throws Exception {
-		signupForm.setUserId(null);
+	void userIdが空文字() throws Exception {
+		signupForm.setUserId("");
 		validator.validate(signupForm,bindingResult);
 		assertThat(bindingResult.getFieldError().getField(),is("userId"));
 		assertThat(bindingResult.getFieldError().getDefaultMessage(),is("must not be blank"));
 	}
+
+	@Test
+	void userIdがEmail形式でない() throws Exception {
+		signupForm.setUserId("sample");
+		validator.validate(signupForm,bindingResult);
+		assertThat(bindingResult.getFieldError().getField(),is("userId"));
+		assertThat(bindingResult.getFieldError().getDefaultMessage(),is("must be a well-formed email address"));
+	}
+
+	@Test
+	void passwordがnull() throws Exception {
+		signupForm.setPassword(null);
+		validator.validate(signupForm,bindingResult);
+		assertThat(bindingResult.getFieldError().getField(),is("password"));
+		assertThat(bindingResult.getFieldError().getDefaultMessage(),is("must not be blank"));
+	}
+
+	@Test
+	void passwordが3文字() throws Exception {
+		signupForm.setPassword("abc");
+		validator.validate(signupForm,bindingResult);
+		assertThat(bindingResult.getFieldError().getField(),is("password"));
+		assertThat(bindingResult.getFieldError().getDefaultMessage(),is("size must be between 4 and 16"));
+	}
+
+	@Test
+	void passwordが17文字() throws Exception {
+		signupForm.setPassword("abcdefghijklmnopq");
+		validator.validate(signupForm,bindingResult);
+		assertThat(bindingResult.getFieldError().getField(),is("password"));
+		assertThat(bindingResult.getFieldError().getDefaultMessage(),is("size must be between 4 and 16"));
+	}
+
+	@Test
+	void userNameが半角スペース() throws Exception {
+		signupForm.setUserName("  ");
+		validator.validate(signupForm,bindingResult);
+		assertThat(bindingResult.getFieldError().getField(),is("userName"));
+		assertThat(bindingResult.getFieldError().getDefaultMessage(),is("must not be blank"));
+	}
+
+	@Test
+	void userNameが21文字() throws Exception {
+		signupForm.setUserName("abcdefghijklmnopqrstu");
+		validator.validate(signupForm,bindingResult);
+		assertThat(bindingResult.getFieldError().getField(),is("userName"));
+		assertThat(bindingResult.getFieldError().getDefaultMessage(),is("size must be between 0 and 20"));
+	}
+
 }
