bb01bc327a13048d2c7cc8eca3978541c5bf3918
yTakaki <yakkosan716@gmail.com>
2020-05-21 16:01:31 +0900

    update SignupController on postLogin method inserting User data

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.example.demo.login.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,9 +10,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.login.domain.model.SignupForm;
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.repository.mybatis.UserMapper;
 
 @Controller
 public class SignupController {
+
+	@Autowired
+	private UserMapper userMapper;
 
 	@GetMapping("/signup")
 	public String getSignup(@ModelAttribute SignupForm form,Model model) {
@@ -25,6 +31,16 @@ public class SignupController {
 			return getSignup(form,model);
 		}
 		System.out.println(form);
+
+		User user = new User();
+
+		user.setUserId(form.getUserId());
+		user.setPassword(form.getPassword());
+		user.setUserName(form.getUserName());
+		boolean result = userMapper.insertUser(user);
+
+		System.out.println(result);
+
 		return "redirect:/login";
 	}
 }
