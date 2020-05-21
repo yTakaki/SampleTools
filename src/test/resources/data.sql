fd8840f6db668869c53367eae09575e9c8d979fc
yTakaki <yakkosan716@gmail.com>
2020-05-21 15:06:33 +0900

    configure h2 database property for unit test

---
+++ src/test/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO login_user (user_id,password,user_name)
+VALUES ('testdata@sample.com','password','testuser');
---
+++ src/test/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS login_user (
+	user_id VARCHAR(50) PRIMARY KEY,
+	password VARCHAR(16) NOT NULL,
+	user_name VARCHAR(20) NOT NULL
+);
