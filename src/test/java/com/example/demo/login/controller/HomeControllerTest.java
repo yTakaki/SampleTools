5f8902111d11b76e76836df40a7c73ed54a0bf73
yTakaki <yakkosan716@gmail.com>
2020-05-23 09:14:09 +0900

    fix SpringBootTest -> WebMvcTest to test faster

--- src/test/java/com/example/demo/login/controller/HomeControllerTest.java
+++ src/test/java/com/example/demo/login/controller/HomeControllerTest.java
@@ -7,12 +7,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest(controllers=HomeController.class)
 public class HomeControllerTest {
 
 	@Autowired
