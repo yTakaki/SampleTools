6c4a849c82fed78a29f9d810c631dfa993220ee9
yTakaki <yakkosan716@gmail.com>
2020-05-25 22:44:00 +0900

    update postUserMaster method to attribute "userList"

--- src/main/java/com/example/demo/manage/controller/UserMasterController.java
+++ src/main/java/com/example/demo/manage/controller/UserMasterController.java
@@ -1,5 +1,9 @@
 package com.example.demo.manage.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -7,10 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.User;
+import com.example.demo.login.domain.service.UserService;
 import com.example.demo.manage.domain.model.UserSearchForm;
 
 @Controller
 public class UserMasterController {
+
+	@Autowired
+	private UserService service;
 
 	@GetMapping("/userMaster")
 	public String getUserMaster(@ModelAttribute UserSearchForm form,Model model) {
@@ -23,7 +32,17 @@ public class UserMasterController {
 		if (bind.hasErrors()) {
 			return getUserMaster(form,model);
 		}
-
+		List<User> userList = new ArrayList<>();
+		if (form.getUserId()==null && form.getUserName()==null) {
+			userList = service.selectAllUser();
+		} else if (form.getUserId()==null) {
+			userList = service.searchUserId(form.getUserId());
+		} else if (form.getUserName()==null) {
+			userList = service.searchUserName(form.getUserName());
+		} else {
+			userList = service.searchUser(form.getUserId(), form.getUserName());
+		}
+		model.addAttribute("userList", userList);
 		return "redirect:/userMaster";
 	}
 }
--- src/test/java/com/example/demo/login/domain/model/SignupFormTest.java
+++ src/test/java/com/example/demo/login/domain/model/SignupFormTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Validator;
 
 @SpringBootTest
+@Transactional
 public class SignupFormTest {
 
 	@Autowired
