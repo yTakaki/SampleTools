c1abd82399c8d8693dcf36f577338be7fa1ccbda
yTakaki <yakkosan716@gmail.com>
2020-05-26 11:19:44 +0900

    add function of updateUser and deleteUser

--- src/main/java/com/example/demo/manage/controller/UserMasterController.java
+++ src/main/java/com/example/demo/manage/controller/UserMasterController.java
@@ -7,12 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.service.UserService;
+import com.example.demo.manage.domain.model.UpdateUserForm;
 import com.example.demo.manage.domain.model.UserSearchForm;
 
 @Controller
@@ -45,6 +48,56 @@ public class UserMasterController {
 		model.addAttribute("userList", userList);
 		model.addAttribute("result", "合計で"+userList.size()+"件のユーザー情報を取得しました。");
 		model.addAttribute("contents", "manage/userMaster :: userMaster_contents");
+		return "login/homeLayout";
+	}
+
+	@GetMapping("/updateUser/{id:.+}")
+	public String getUpdateUser(@ModelAttribute UpdateUserForm form,Model model,
+			@PathVariable("id") String userId) {
+		System.out.println("userId="+userId);
+		model.addAttribute("contents", "manage/updateUser :: updateUser_contents");
+		if (userId!=null && userId.length()>0) {
+			User user = service.selectOneUser(userId);
+			form.setUserId(user.getUserId());
+			form.setUserName(user.getUserName());
+			model.addAttribute("updateUserForm", form);
+		}
+		return "login/homeLayout";
+	}
+
+	@PostMapping("/updateUser")
+	public String postUpdateUser(@ModelAttribute @Validated UpdateUserForm form,BindingResult bind,Model model) {
+		if (bind.hasErrors()) {
+			return getUpdateUser(form,model,form.getUserId());
+		}
+		System.out.println(form);
+		model.addAttribute("contents", "manage/userMaster :: userMaster_contents");
+
+		User user = service.selectOneUser(form.getUserId());
+		user.setUserName(form.getUserName());
+		boolean result = service.updateUser(user);
+		if (result==true) {
+			System.out.println("update success.");
+			model.addAttribute("result","ユーザー名("+form.getUserName()+")を更新しました。");
+		} else {
+			System.out.println("update failure.");
+			model.addAttribute("result","ユーザー名("+form.getUserName()+")の更新に失敗しました。");
+		}
+		return "login/homeLayout";
+	}
+
+	@GetMapping("/deleteUser/{id:.+}")
+	public String getDeleteUser(Model model,@PathVariable("id") String userId) {
+		System.out.println("userId="+userId);
+		model.addAttribute("contents","manage/userMaster :: userMaster_contents");
+		boolean result = service.deleteUser(userId);
+		if (result==true) {
+			System.out.println("delete success.");
+			model.addAttribute("result","ユーザーID("+userId+")を更新しました。");
+		} else {
+			System.out.println("delete failure.");
+			model.addAttribute("result","ユーザーID("+userId+")の更新に失敗しました。");
+		}
 		return "login/homeLayout";
 	}
 }
