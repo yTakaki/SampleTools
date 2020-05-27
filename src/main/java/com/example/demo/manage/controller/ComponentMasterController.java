21250ac4b77cb5d7320b5063345a874088d196bf
yTakaki <yakkosan716@gmail.com>
2020-05-27 17:20:41 +0900

    add radio button to configure componentStatus

--- src/main/java/com/example/demo/manage/controller/ComponentMasterController.java
+++ src/main/java/com/example/demo/manage/controller/ComponentMasterController.java
@@ -1,15 +1,42 @@
 package com.example.demo.manage.controller;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.demo.manage.domain.model.ComponentSearchForm;
 
 @Controller
 public class ComponentMasterController {
 
+	private Map<String,Integer> radioComponentStatus;
+
+	private Map<String,Integer> initRadioComponentStatus() {
+		Map<String,Integer> radio = new LinkedHashMap<>();
+		radio.put("取扱い可能", 0);
+		radio.put("終売予定", 1);
+		radio.put("終了", 2);
+		return radio;
+	}
 	@GetMapping("/componentMaster")
-	public String getComponentMaster(Model model) {
+	public String getComponentMaster(@ModelAttribute ComponentSearchForm form,Model model) {
+		radioComponentStatus = initRadioComponentStatus();
+		model.addAttribute("radioComponentStatus",radioComponentStatus);
+
 		model.addAttribute("contents","manage/componentMaster :: componentMaster_contents");
+		return "login/homeLayout";
+	}
+
+	@GetMapping("/registComponent")
+	public String getRegistComponent(Model model) {
+		radioComponentStatus = initRadioComponentStatus();
+		model.addAttribute("radioComponentStatus",radioComponentStatus);
+
+		model.addAttribute("contents","manage/registComponent :: componentRegist_contents");
 		return "login/homeLayout";
 	}
 }
