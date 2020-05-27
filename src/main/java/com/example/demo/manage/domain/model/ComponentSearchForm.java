479ed207afa6c6947d9fa3a4e29f8208150caea9
yTakaki <yakkosan716@gmail.com>
2020-05-27 17:20:07 +0900

    add ComponentSearchForm

---
+++ src/main/java/com/example/demo/manage/domain/model/ComponentSearchForm.java
@@ -0,0 +1,15 @@
+package com.example.demo.manage.domain.model;
+
+import lombok.Data;
+
+@Data
+public class ComponentSearchForm {
+
+	private String componentId;
+
+	private String componentCd;
+
+	private String componentName;
+
+	private int componentStatus;
+}
