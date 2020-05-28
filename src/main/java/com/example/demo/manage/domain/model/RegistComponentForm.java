7fdf75fe3ca2f0bf0fc5e6fe76ab592b2393d9c7
yTakaki <yakkosan716@gmail.com>
2020-05-28 16:36:23 +0900

    add RegistComponentFormTest all green

--- src/main/java/com/example/demo/manage/domain/model/RegistComponentForm.java
+++ src/main/java/com/example/demo/manage/domain/model/RegistComponentForm.java
@@ -1,14 +1,23 @@
 package com.example.demo.manage.domain.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class RegistComponentForm {
 
+	@NotBlank
+	@Size(max=8)
 	private String componentId;
 
+	@NotBlank
+	@Size(max=20)
 	private String componentCd;
 
+	@NotBlank
+	@Size(max=50)
 	private String componentName;
 
 	private boolean foodFlag;
