a424b82c87b730214b72a11ac9ed80a95918f0e5
yTakaki <yakkosan716@gmail.com>
2020-05-28 13:06:32 +0900

    add componentList button of all components

--- src/main/java/com/example/demo/manage/controller/ComponentMasterController.java
+++ src/main/java/com/example/demo/manage/controller/ComponentMasterController.java
@@ -1,17 +1,24 @@
 package com.example.demo.manage.controller;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import com.example.demo.manage.domain.model.Component;
 import com.example.demo.manage.domain.model.ComponentSearchForm;
+import com.example.demo.manage.domain.service.ComponentService;
 
 @Controller
 public class ComponentMasterController {
+
+	@Autowired
+	private ComponentService service;
 
 	private Map<String,Boolean> radioFoodFlag;
 	private Map<String,Integer> radioComponentStatus;
@@ -42,6 +49,20 @@ public class ComponentMasterController {
 		return "login/homeLayout";
 	}
 
+	@GetMapping("/componentList")
+	public String getComponentList(@ModelAttribute ComponentSearchForm form,Model model) {
+		radioFoodFlag = initRadioFoodFlag();
+		model.addAttribute("radioFoodFlag", radioFoodFlag);
+		radioComponentStatus = initRadioComponentStatus();
+		model.addAttribute("radioComponentStatus",radioComponentStatus);
+
+		List<Component> componentList = service.selectAllComponent();
+		System.out.println(componentList);
+		model.addAttribute("result","合計"+componentList.size()+"件の構成品を抽出しました。");
+		model.addAttribute("componentList", componentList);
+		model.addAttribute("contents","manage/componentMaster :: componentMaster_contents");
+		return "login/homeLayout";
+	}
 	@GetMapping("/registComponent")
 	public String getRegistComponent(Model model) {
 		radioFoodFlag = initRadioFoodFlag();
