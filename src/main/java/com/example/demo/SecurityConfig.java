b6738376accb180eb781619e5e903089cff3e1bf
yTakaki <yakkosan716@gmail.com>
2020-06-03 13:20:00 +0900

    add Login and Logout function

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,14 +1,35 @@
 package com.example.demo;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	// データソース
+	@Autowired
+	private DataSource dataSource;
+
+	// ユーザーIDとパスワードを取得するSQL
+	private static final String USER_SQL = "SELECT user_id, password, true FROM login_user WHERE user_id = ?";
+	private static final String ROLE_SQL = "SELECT user_id, 'ROLE_USER' FROM login_user WHERE user_id = ?";
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
@@ -17,14 +38,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+
+		// 直リンク設定（ログイン不要）
 		http.authorizeRequests().antMatchers("/webjars/**").permitAll()
 		.antMatchers("/css/**").permitAll()
 		.antMatchers("/login").permitAll()
-		.antMatchers("/signup").permitAll()
-		.antMatchers("/**").permitAll() // ログイン実装までの許可
+		.antMatchers("/signup").permitAll() //.antMatchers("/**").permitAll() // ログイン実装までの許可
 		.anyRequest().authenticated();
 
+		// ログイン処理
+		http.formLogin()
+		.loginProcessingUrl("/login")
+		.loginPage("/login")
+		.failureUrl("/login")
+		.usernameParameter("userId")
+		.passwordParameter("password")
+		.defaultSuccessUrl("/home", true);
+
+		// ログアウト処理
+		http.logout()
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutUrl("/logout")
+		.logoutSuccessUrl("/login");
+
 		http.csrf().disable(); // 一時的に無効
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// ログイン処理時のユーザー情報をDBから取得
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.usersByUsernameQuery(USER_SQL)
+		.authoritiesByUsernameQuery(ROLE_SQL)
+		.passwordEncoder(passwordEncoder());
 	}
 
 }
--- src/main/java/com/example/demo/login/domain/service/UserService.java
+++ src/main/java/com/example/demo/login/domain/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.login.domain.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +15,13 @@ import com.example.demo.login.domain.repository.UserMapper;
 public class UserService {
 
 	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
 	UserMapper mapper;
 
 	public boolean insertUser(User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		return mapper.insertUser(user);
 	}
 
@@ -29,6 +34,7 @@ public class UserService {
 	}
 
 	public boolean updateUser(User user) {
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		return mapper.updateUser(user);
 	}
 
