9caf8c9b1b4dcd10a5cac8630e2dbff73b03f135
yTakaki <yakkosan716@gmail.com>
2020-06-05 21:45:33 +0900

    trying login function test

--- src/test/java/com/example/demo/login/controller/LoginControllerTest.java
+++ src/test/java/com/example/demo/login/controller/LoginControllerTest.java
@@ -1,30 +1,43 @@
 package com.example.demo.login.controller;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.demo.login.domain.service.UserDetailsServiceImpl;
-
-@WebMvcTest(controllers = LoginController.class)
+//@WebMvcTest(controllers = LoginController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("classpath:test.properties")
 public class LoginControllerTest {
 
 	@Autowired
 	private MockMvc mock;
 
-	@MockBean
-	private UserDetailsServiceImpl service;
+	//@MockBean
+	//private UserDetailsServiceImpl service;
 
 	@Test
 	void ログイン画面へのリクエストに対して正常な画面が返されること() throws Exception {
 		mock.perform(get("/login"))
 		.andExpect(status().isOk())
 		.andExpect(view().name("login/login"));
+	}
+
+	// loginTestをしない場合は、@WebMvcTestでOK
+	@Test
+	@WithMockUser
+	void loginTest() throws Exception {
+		mock.perform(formLogin("/login")) // 現状、ユーザー情報が誤りという扱い
+		.andExpect(status().isFound())
+		.andExpect(redirectedUrl("/home"));
 	}
 
 }
