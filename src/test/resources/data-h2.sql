1f63cc7c408eb7cd0e49ba22171892e8a8db2c1f
yTakaki <yakkosan716@gmail.com>
2020-06-08 17:42:34 +0900

    refactoring schema and data sql for test

--- src/test/resources/data-h2.sql
+++ src/test/resources/data-h2.sql
@@ -1,8 +1,3 @@
 INSERT INTO login_user
 VALUES ('testdata@sample.com','$2a$10$cPIC6VUsJfeSmWDG86k.6.lgMsQK.FMN.FWNLffcOxIPu.HTGv0XK','testuser');
 
-INSERT INTO component VALUES ('00000001','TestA','testcomponentA',true,0);
-INSERT INTO component VALUES ('00000002','TestB','testcomponentB',true,0);
-
-INSERT INTO item(item_id,item_cd,item_name,composite_flag,item_status,comp1,comp2)
-VALUES ('00000001','Test','testitem',true,0,'00000001','00000002');
