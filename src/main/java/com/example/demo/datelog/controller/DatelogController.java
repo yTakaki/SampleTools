a7c53b6abb025660d73a7b4d20168cd3801ef7b3
yTakaki <yakkosan716@gmail.com>
2020-06-09 13:06:48 +0900

    creating datelog view

--- src/main/java/com/example/demo/datelog/controller/DatelogController.java
+++ src/main/java/com/example/demo/datelog/controller/DatelogController.java
@@ -7,21 +7,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DatelogController {
 
-	// 賞味期限管理画面へのリクエスト
-		@GetMapping("/datelog")
-		public String getDatelogSearch(Model model) {
+	// 賞味期限管理ホーム画面へのリクエスト
+	@GetMapping("/datelog")
+	public String getDatelogSearch(Model model) {
 
-			model.addAttribute("contents","datelog/datelogSearch :: datelogSearch_contents");
-			return "login/homeLayout";
-		}
-
-	// 賞味期限記録画面へのリクエスト
-	@GetMapping("/datelogRegist")
-	public String getDatelogRegist(Model model) {
-
-		model.addAttribute("contents","datelog/datelogRegist :: datelogRegist_contents");
+		model.addAttribute("contents","datelog/datelogHome :: datelogHome_contents");
 		return "login/homeLayout";
 	}
 
+	// セットアップ品用日付登録画面へのリクエスト
+	@GetMapping("/datelogRegistProduct")
+	public String getDatelogRegistProduct(Model model) {
+
+		model.addAttribute("contents","datelog/datelogRegistProduct :: datelogRegistProduct_contents");
+		return "login/homeLayout";
+	}
+
+	// 既製品用日付登録画面へのリクエスト
+	@GetMapping("/datelogRegistComp")
+	public String getDatelogRegistComp(Model model) {
+
+		model.addAttribute("contents","datelog/datelogRegistComp :: datelogRegistComp_contents");
+		return "login/homeLayout";
+	}
+	// セットアップ品用履歴検索画面へのリクエスト
+	@GetMapping("/datelogSearchProduct")
+	public String getDatelogSearchProduct(Model model) {
+
+		model.addAttribute("contents","datelog/datelogSearchProduct :: datelogSearchProduct_contents");
+		return "login/homeLayout";
+	}
+
+	// 既製品用履歴検索画面へのリクエスト
+	@GetMapping("/datelogSearchComp")
+	public String getDatelogSearchComp(Model model) {
+
+		model.addAttribute("contents","datelog/datelogSearchComp :: datelogSearchComp_contents");
+		return "login/homeLayout";
+	}
 
 }
